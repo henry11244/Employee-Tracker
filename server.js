bd3a8682968b030f547a27aa7699331f929f52e7
Henry Nguyen <hln11244@gmail.com>
2022-07-27 22:25:58 -0700

    edit employee role function operational

--- server.js
+++ server.js
@@ -40,9 +40,69 @@ viewAllEmployees = () => {
     init()
 }
 
-addEmployees = () => { }
+addEmployees = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'fName',
+            message: `What is this employee's first name?`,
 
-updatedEmployeeRole = () => { }
+        },
+        {
+            type: 'input',
+            name: 'lName',
+            message: `What is this employee's last name?`,
+
+        },
+        {
+            type: 'input',
+            name: 'managerID',
+            message: `What is this employee's manager's ID?`,
+
+        },
+        {
+            type: 'input',
+            name: 'roleID',
+            message: `What is this employee's role ID?`,
+
+        },
+
+    ])
+        .then((answer) => {
+            console.log(answer.Answer)
+            db.query(`insert into employee (first_name, last_name, manager_id, role_id) values('${answer.fName}', '${answer.lName}', ${answer.managerID}, ${answer.roleID});`, (err, result) => {
+                db.query('select * from employee;', (err, result) => {
+                    console.table(result)
+                });
+            });
+            init()
+        })
+}
+
+updatedEmployeeRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: `What is the ID of the employee you wish to update?`,
+
+        },
+        {
+            type: 'input',
+            name: 'newRoleID',
+            message: `What is the new role ID for this employee?`,
+
+        },
+    ])
+        .then((answer) => {
+            db.query(`update employee set role_id = ${answer.newRoleID} where id =${answer.employeeID} ;`, (err, result) => {
+                db.query('select * from employee;', (err, result) => {
+                    console.table(result)
+                });
+            });
+            init()
+        })
+}
 
 viewAllRoles = () => {
     db.query('select title as job_title, role.id AS role_id, department.name AS department_name, salary from role JOIN department ON role.department_id = department.id;', (err, result) => {
@@ -51,7 +111,38 @@ viewAllRoles = () => {
     init()
 }
 
-addRoles = () => { }
+addRoles = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: `What is this role's title?`,
+
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: `What is this role's salary?`,
+
+        },
+        {
+            type: 'input',
+            name: 'departmentID',
+            message: `What is this role's department ID?`,
+
+        },
+
+    ])
+        .then((answer) => {
+            console.log(answer.Answer)
+            db.query(`insert into role (title, salary, department_id) values('${answer.title}', ${answer.salary}, ${answer.departmentID});`, (err, result) => {
+                db.query('select * from role;', (err, result) => {
+                    console.table(result)
+                });
+            });
+            init()
+        })
+}
 
 viewAllDepartments = () => {
     db.query('select name AS department_name, id AS department_id from department;', (err, result) => {
@@ -97,13 +188,13 @@ function init() {
             switch (answers.Answer) {
                 case intialChoices[0]: viewAllEmployees();
                     break;
-                case intialChoices[1]: console.log("test2");
+                case intialChoices[1]: addEmployees();
                     break;
-                case intialChoices[2]: console.log("test3");
+                case intialChoices[2]: updatedEmployeeRole();
                     break;
                 case intialChoices[3]: viewAllRoles();
                     break;
-                case intialChoices[4]: console.log("test5");
+                case intialChoices[4]: addRoles();
                     break;
                 case intialChoices[5]: viewAllDepartments();
                     break;
